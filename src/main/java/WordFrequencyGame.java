c665e1e45e0cb71470b70eda0e4da4edc4f5ad2a
CHEUNJO2 <cheunjo2@oocl.com>
2021-12-06 16:49:04 +0800

    [WordFrequency] [joanne] refractor: rename unknown variable to WordInfo

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -5,26 +5,24 @@ import java.util.Map;
 import java.util.StringJoiner;
 
 public class WordFrequencyGame {
-    private static String SPACE_PATTERN = "\\s+";
+    private final static String SPACE_PATTERN = "\\s+";
     public String getResult(String inputStr){
 
-        if (inputStr.split(SPACE_PATTERN).length==1) {
+        if (inputStr.split(SPACE_PATTERN).length == 1) {
             return inputStr + " 1";
         } else {
-
             try {
-
                 //split the input string with 1 to n pieces of spaces
                 String[] words = inputStr.split(SPACE_PATTERN);
 
                 List<WordInfo> wordInfoList = new ArrayList<>();
-                for (String s : words) {
-                    WordInfo wordInfo = new WordInfo(s, 1);
+                for (String word : words) {
+                    WordInfo wordInfo = new WordInfo(word, 1);
                     wordInfoList.add(wordInfo);
                 }
 
                 //get the map for the next step of sizing the same word
-                Map<String, List<WordInfo>> map =getListMap(wordInfoList);
+                Map<String, List<WordInfo>> map = getListMap(wordInfoList);
 
                 List<WordInfo> list = new ArrayList<>();
                 for (Map.Entry<String, List<WordInfo>> entry : map.entrySet()){
@@ -36,9 +34,9 @@ public class WordFrequencyGame {
                 wordInfoList.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
 
                 StringJoiner joiner = new StringJoiner("\n");
-                for (WordInfo w : wordInfoList) {
-                    String s = w.getValue() + " " +w.getWordCount();
-                    joiner.add(s);
+                for (WordInfo wordInfo : wordInfoList) {
+                    String wordFrequencyCountString = wordInfo.getValue() + " " + wordInfo.getWordCount();
+                    joiner.add(wordFrequencyCountString);
                 }
                 return joiner.toString();
             } catch (Exception e) {
@@ -52,7 +50,7 @@ public class WordFrequencyGame {
 
     private Map<String,List<WordInfo>> getListMap(List<WordInfo> wordInfoList) {
         Map<String, List<WordInfo>> map = new HashMap<>();
-        for (WordInfo wordInfo : wordInfoList){
+        for (WordInfo wordInfo : wordInfoList) {
 //       map.computeIfAbsent(input.getValue(), k -> new ArrayList<>()).add(input);
             if (!map.containsKey(wordInfo.getValue())){
                 ArrayList arr = new ArrayList<>();
